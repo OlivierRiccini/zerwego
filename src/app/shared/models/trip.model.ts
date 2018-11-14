e0b002cf6dceb9e6d5ed216e6aeb982751077761
Olivier Riccini <olivier-webdev@hotmail.com>
2018-11-14 18:30:20 -0500

    Add trip model and trip service

---
+++ src/app/shared/models/trip.model.ts
@@ -0,0 +1,7 @@
+export class Trip {
+    constructor(tripName: string, 
+                destination: string, 
+                dateStart: Date, 
+                dateEnd: Date, 
+                participants: number) {}
+}
