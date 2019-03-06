e465b0a72b5874905d0d8761d9014b701d63b62e
Olivier Riccini <olivier-webdev@hotmail.com>
2019-03-06 18:57:33 -0500

    Working UX landing page

--- src/app/home/home-signup.component.ts
+++ src/app/home/home-signup.component.ts
@@ -1,48 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
-import { AuthComponent } from '../auth/auth.component';
-import { MatDialog, MatDialogRef } from '@angular/material';
-import { Router } from '@angular/router';
-import { HomeComponent } from '../home/home.component';
 
 @Component({
   selector: 'app-home-signup',
   templateUrl: './home-signup.component.html',
   styleUrls: ['./home-signup.component.scss']
 })
-export class HomeSignupComponent extends AuthComponent implements OnInit {
+export class HomeSignupComponent implements OnInit {
 
-  authForm: FormGroup;
+  signupForm: FormGroup;
 
   constructor(
     public fb: FormBuilder,
-    public authService: AuthService,
-    public dialog: MatDialog,
-    public dialogRef: MatDialogRef<HomeComponent>,
-    public router: Router) { 
-      super(fb, authService, dialogRef, router)
+    public authService: AuthService
+    ) { 
+        this.signupForm = this.fb.group({
+            name: [''],
+            email: [''],
+            password: ['']
+        });
   }
   
   ngOnInit() {
-    super.ngOnInit();
-    this.signUpMode = true;
-    this.label = {
-      title: 'Sign up',
-      submit: 'Sign up',
-      changeForm: 'I already have an account'
-    }; 
-    this.authForm.addControl('name', new FormControl(''));
+    
   }
 
   public onSubmit() {
-    const user = this.authForm.value;
+    const user = this.signupForm.value;
     this.authService.register(user).subscribe(
       user => {
-        this.dialogRef.close();
         console.log(user);
       },
-      err => console.log('Error= ' + err)
+      err => console.log(err)
     )
   }
 }
