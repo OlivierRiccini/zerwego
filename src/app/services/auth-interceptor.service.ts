f78808104c0fc36b99158c231ae5b2d74921ff6f
Olivier Riccini <olivier-webdev@hotmail.com>
2019-05-11 23:02:55 -0400

    work in progress

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -1,45 +1,108 @@
-import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders, HttpResponse, HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
- 
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { Observable, throwError, BehaviorSubject } from 'rxjs';
+import { catchError, filter, take, switchMap } from 'rxjs/operators';
+
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
- 
-  constructor(private auth: AuthService) {
-  }
-  
-  intercept(req: HttpRequest<any>,
-    next: HttpHandler): Observable<HttpEvent<any>> {
 
-    const token = this.auth.getTokens();
-    console.log(token);
-    if (token) {
-      const cloned = req.clone({
-        headers: new HttpHeaders({
-          'Authorization': `Bearer ${token}`
-        })
-      });
+  private isRefreshing = false;
+  private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
+  constructor(public authService: AuthService) { }
 
-      return next.handle(cloned).pipe(
-        tap((ev: HttpEvent<any>) => {
-          if (ev instanceof HttpResponse) {
-            const token: string = ev.headers.get('authorization');
-            if (token) {
-              this.auth.refreshToken(token);
-            }
-          }
-        }, err => {
-          if (err.error.name === 'TokenExpiredError'
-              || err.error.message === 'Refresh token is expired, user has to login') {
-            this.auth.autoLogout();
-          }
-        }))
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    if (this.authService.getJwtToken()) {
+      request = this.addToken(request, this.authService.getJwtToken());
     }
-    else {
-      return next.handle(req);
+
+    return next.handle(request).pipe(catchError(error => {
+      if (error instanceof HttpErrorResponse && error.status === 401) {
+        return this.handle401Error(request, next);
+      } else {
+        return throwError(error);
+      }
+    }));
+  }
+
+  private addToken(request: HttpRequest<any>, token: string) {
+    return request.clone({
+      setHeaders: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+  }
+
+  private handle401Error(request: HttpRequest<any>, next: HttpHandler) {
+    if (!this.isRefreshing) {
+      this.isRefreshing = true;
+      this.refreshTokenSubject.next(null);
+
+      return this.authService.refreshToken().pipe(
+        switchMap((response: any) => {
+          const jwt = response.headers.get('jwt');
+          this.isRefreshing = false;
+          this.refreshTokenSubject.next(jwt);
+          return next.handle(this.addToken(request, jwt));
+        }));
+
+    } else {
+      return this.refreshTokenSubject.pipe(
+        filter(token => token != null),
+        take(1),
+        switchMap(jwt => {
+          return next.handle(this.addToken(request, jwt));
+        }));
     }
   }
 }
+
+
+// import { AuthService } from './auth.service';
+// import { Injectable } from '@angular/core';
+// import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpHeaders, HttpResponse } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { tap } from 'rxjs/operators';
+ 
+// @Injectable()
+// export class AuthInterceptor implements HttpInterceptor {
+ 
+//   constructor(private auth: AuthService) {
+//     console.log('AuthInterceptor');
+//     console.log(this.auth);
+//   }
+  
+//   intercept(req: HttpRequest<any>,
+//     next: HttpHandler): Observable<HttpEvent<any>> {
+
+//     const token = this.auth.getTokens();
+//     if (token) {
+//       const cloned = req.clone({
+//         headers: new HttpHeaders({
+//           'Authorization': `Bearer ${token}`
+//         })
+//       });
+
+
+//       return next.handle(cloned).pipe(
+//         tap((ev: HttpEvent<any>) => {
+//           if (ev instanceof HttpResponse) {
+//             const token: string = ev.headers.get('authorization');
+//             if (token) {
+//               this.auth.refreshAccessToken(token);
+//             }
+//           }
+//         }, err => {
+//           if (err.error.name === 'TokenExpiredError'
+//               || err.error.message === 'Refresh token is expired, user has to login') {
+//             this.auth.autoLogout();
+//           }
+//         }))
+//     }
+//     else {
+//       return next.handle(req);
+//     }
+//   }
+// }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,108 +1,263 @@
 import { Injectable, EventEmitter } from '@angular/core';
-import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { Observable, of } from 'rxjs';
-import { IUser } from '../models/user';
-import { Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { of, Observable } from 'rxjs';
+import { catchError, mapTo, tap } from 'rxjs/operators';
+// import { config } from './../../config';
 import * as jwt_decode from "jwt-decode";
 import { UserInterfaceService } from './user-interface.service';
 import { ICredentials, IForgotPassword } from '../models/auth';
+import { IUser } from '../models/user';
+import { Router } from '@angular/router';
 
-const baseUrl = 'http://localhost:3000/auth';
+const config = { apiUrl: 'http://localhost:3000/auth'};
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthService {
 
-  public loggedObs = new EventEmitter<IUser>();
+  private readonly JWT_TOKEN = 'JWT_TOKEN';
+  private readonly REFRESH_TOKEN = 'REFRESH_TOKEN';
+  private readonly CURRENT_USER = 'CURRENT_USER';
+
   public switchDialogEvent = new EventEmitter<boolean>();
+  public userLoggedEvent = new EventEmitter<IUser>();
 
   constructor(private http: HttpClient, private router: Router, private userInterfaceService: UserInterfaceService) { }
 
-  register(user: IUser): Observable<HttpResponse<Object>> {
-    return this.http.post<any>(`${baseUrl}/register`, user, { observe: 'response' })
-      .pipe(map(response => {
-        let user;
-        const token = response.headers.get('authorization');
-        // const refreshToken = response.headers.get('refresh_token');
-        if (token) {
-          var decoded = jwt_decode(token);
-          user = decoded.payload;
-          localStorage.setItem('Authorization', token);
-          // localStorage.setItem('Refresh_token', refreshToken);
-          localStorage.setItem('currentUser', JSON.stringify(user));
-          this.loggedObs.emit(user);
-        }
-        return user;
+  public register(user: IUser): Observable<boolean> {
+    return this.http.post<any>(`${config.apiUrl}/register`, user, { observe: 'response' })
+      .pipe(
+        tap(response => {
+          const jwt = response.headers.get('jwt');
+          const refreshToken = response.headers.get('refreshToken');
+          this.doLoginUser({jwt, refreshToken});
+        }),
+        mapTo(true),
+        catchError(error => {
+          alert(error.error);
+          return of(false);
+        }));
+  }
+
+  public login(credentials: ICredentials): Observable<boolean> {
+    return this.http.post<any>(`${config.apiUrl}/login`, credentials, { observe: 'response' })
+      .pipe(
+        tap(response => {
+          const jwt = response.headers.get('jwt');
+          const refreshToken = response.headers.get('refreshToken');
+          this.doLoginUser({jwt, refreshToken});
+        }),
+        mapTo(true),
+        catchError(error => {
+          console.log(error);
+          alert(error.error);
+          return of(false);
+        }));
+  }
+
+  public logout(): Observable<boolean> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'refreshToken': this.getRefreshToken() 
+    });
+    const options = { headers: headers };
+    return this.http.post<any>(`${config.apiUrl}/logout`, null, options).pipe(
+      tap(() => this.doLogoutUser()),
+      mapTo(true),
+      catchError(error => {
+        alert(error.error);
+        return of(false);
       }));
   }
 
-  login(credentials: ICredentials) {
-    return this.http.post<any>(`${baseUrl}/login`, credentials, { observe: 'response' })
-      .pipe(map(response => {
-        let user: IUser;
-        const accesToken = response.headers.get('authorization');
-        if (accesToken) {
-          var decoded = jwt_decode(accesToken);
-          user = decoded.payload;
-          localStorage.setItem('Authorization', accesToken);
-          localStorage.setItem('currentUser', JSON.stringify(user));
-        }
-        this.loggedObs.emit(user);
-        return user;
-      }));
+  public isLoggedIn() {
+    return !!this.getJwtToken();
   }
 
-  logout() {
-    this.http.delete(`${baseUrl}/logout`).subscribe(
-      () => {
-        localStorage.removeItem('Authorization');
-        localStorage.removeItem('currentUser');
-        this.loggedObs.emit(null);
-        this.router.navigate(['/']);
-        this.userInterfaceService.success('Successfully logedOut');
-      },
-      err => this.userInterfaceService.error(err)
-    );
+  public refreshToken() {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'refreshToken': this.getRefreshToken() 
+    });
+    const options = { headers: headers, observe: 'response' as 'body' };
+    return this.http.post<any>(`${config.apiUrl}/refresh`, null, options,).pipe(
+      tap((response: any) => {
+        const jwt = response.headers.get('jwt');
+        const refreshToken = response.headers.get('refreshToken'); 
+        this.storeJwtToken(jwt);
+    }));
   }
 
-  autoLogout() {
-    this.logout();
-    this.userInterfaceService.confirm({
-      message: 'Session expired, let\'s login!',
-      trueLabel: 'Let\'s login',
-      falseLabel: 'No thanks'
-    }).subscribe(
-      response => response ? this.router.navigate(['/', 'signin']) : this.router.navigate(['./'])
-    );
+  public forgotPassword(contact: IForgotPassword) {
+    return this.http.post<any>(`${config.apiUrl}/forgot-password`, contact);
   }
 
-  refreshToken(token: string) {
-    localStorage.setItem('Authorization', token);
+  public getJwtToken() {
+    return localStorage.getItem(this.JWT_TOKEN);
   }
 
-  getToken(): string {
-    return localStorage.getItem('Authorization');
-  }
-
-  getTokens() {
-    return localStorage.getItem('Authorization')
-  }
-
-  getCurrentUser(): IUser {
-    const user = localStorage.getItem('currentUser');
+  public getCurrentUser(): IUser {
+    const user = localStorage.getItem(this.CURRENT_USER);
     return user ? JSON.parse(user) : false;
   }
 
-  isLoggedIn(): Observable<boolean> {
-    return (this.getToken() && this.getToken() !== 'undefined' && this.getCurrentUser()) ? of(true) : of(false);
-  }
-
-  switchDialog(isAModeChange: boolean) {
+  public switchDialog(isAModeChange: boolean) {
     this.switchDialogEvent.emit(isAModeChange);
   }
 
-  forgotPassword(contact: IForgotPassword) {
-    return this.http.post<any>(`${baseUrl}/forgot-password`, contact);
+  private doLoginUser(tokens: any) {
+    const decoded = jwt_decode(tokens.jwt);
+    const user = decoded.payload;
+    this.storeCurrentUser(user);
+    this.userLoggedEvent.emit(user);
+    this.storeTokens(tokens);
+  }
+
+  private doLogoutUser() {
+    this.removeCurrentUser();
+    this.removeTokens();
+    this.userLoggedEvent.emit(null);
+    this.userInterfaceService.success('Successfully logedOut');
+  }
+
+  private getRefreshToken() {
+    return localStorage.getItem(this.REFRESH_TOKEN);
+  }
+
+  private storeJwtToken(jwt: string) {
+    localStorage.setItem(this.JWT_TOKEN, jwt);
+  }
+
+  private storeTokens(tokens: any) {
+    localStorage.setItem(this.JWT_TOKEN, tokens.jwt);
+    localStorage.setItem(this.REFRESH_TOKEN, tokens.refreshToken);
+  }
+
+  private storeCurrentUser(user: IUser) {
+    localStorage.setItem(this.CURRENT_USER, JSON.stringify(user));
+  }
+
+  private removeTokens() {
+    localStorage.removeItem(this.JWT_TOKEN);
+    localStorage.removeItem(this.REFRESH_TOKEN);
+  }
+
+  private removeCurrentUser() {
+    localStorage.removeItem(this.CURRENT_USER);  
   }
 
 }
+
+
+
+// import { Injectable, EventEmitter } from '@angular/core';
+// import { HttpClient, HttpResponse, HttpHeaders } from '@angular/common/http';
+// import { map } from 'rxjs/operators';
+// import { Observable, of } from 'rxjs';
+// import { IUser } from '../models/user';
+// import { Router } from '@angular/router';
+// import * as jwt_decode from "jwt-decode";
+// import { UserInterfaceService } from './user-interface.service';
+// import { ICredentials, IForgotPassword } from '../models/auth';
+
+// const baseUrl = 'http://localhost:3000/auth';
+
+// @Injectable()
+// export class AuthService {
+
+//   public userLoggedEvent = new EventEmitter<IUser>();
+//   public switchDialogEvent = new EventEmitter<boolean>();
+
+//   constructor(private http: HttpClient, private router: Router, private userInterfaceService: UserInterfaceService) { }
+
+//   register(user: IUser): Observable<HttpResponse<Object>> {
+//     return this.http.post<any>(`${baseUrl}/register`, user, { observe: 'response' })
+//       .pipe(map(response => {
+//         let user;
+//         const token = response.headers.get('authorization');
+//         // const refreshToken = response.headers.get('refresh_token');
+//         if (token) {
+//           var decoded = jwt_decode(token);
+//           user = decoded.payload;
+//           localStorage.setItem('Authorization', token);
+//           // localStorage.setItem('Refresh_token', refreshToken);
+//           localStorage.setItem('currentUser', JSON.stringify(user));
+//           this.userLoggedEvent.emit(user);
+//         }
+//         return user;
+//       }));
+//   }
+
+//   login(credentials: ICredentials) {
+//     return this.http.post<any>(`${baseUrl}/login`, credentials, { observe: 'response' })
+//       .pipe(map(response => {
+//         let user: IUser;
+//         const accesToken = response.headers.get('authorization');
+//         if (accesToken) {
+//           var decoded = jwt_decode(accesToken);
+//           user = decoded.payload;
+//           localStorage.setItem('Authorization', accesToken);
+//           localStorage.setItem('currentUser', JSON.stringify(user));
+//         }
+//         this.userLoggedEvent.emit(user);
+//         return user;
+//       }));
+//   }
+
+//   logout() {
+//     this.http.delete(`${baseUrl}/logout`).subscribe(
+//       () => {
+//         localStorage.removeItem('Authorization');
+//         localStorage.removeItem('currentUser');
+//         this.userLoggedEvent.emit(null);
+//         this.router.navigate(['/']);
+//         this.userInterfaceService.success('Successfully logedOut');
+//       },
+//       err => this.userInterfaceService.error(err)
+//     );
+//   }
+
+//   autoLogout() {
+//     this.logout();
+//     this.userInterfaceService.confirm({
+//       message: 'Session expired, let\'s login!',
+//       trueLabel: 'Let\'s login',
+//       falseLabel: 'No thanks'
+//     }).subscribe(
+//       response => response ? this.router.navigate(['/', 'signin']) : this.router.navigate(['./'])
+//     );
+//   }
+
+//   refreshAccessToken(token: string) {
+//     console.log('Refreshing token... new token :');
+//     console.log(token);
+//     localStorage.setItem('Authorization', token);
+//   }
+
+//   getToken(): string {
+//     return localStorage.getItem('Authorization');
+//   }
+
+//   getTokens() {
+//     return localStorage.getItem('Authorization')
+//   }
+
+//   getCurrentUser(): IUser {
+//     const user = localStorage.getItem('currentUser');
+//     return user ? JSON.parse(user) : false;
+//   }
+
+//   isLoggedIn(): Observable<boolean> {
+//     return (this.getToken() && this.getToken() !== 'undefined' && this.getCurrentUser()) ? of(true) : of(false);
+//   }
+
+//   switchDialog(isAModeChange: boolean) {
+//     this.switchDialogEvent.emit(isAModeChange);
+//   }
+
+//   forgotPassword(contact: IForgotPassword) {
+//     return this.http.post<any>(`${baseUrl}/forgot-password`, contact);
+//   }
+
+// }
